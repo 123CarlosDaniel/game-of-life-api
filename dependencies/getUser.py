e0fb77d6dd2883b8f86c44ceb6e19c9e3a52adf9
cdcm <carloss_jt@hotmail.com>
2024-08-16 00:46:44 -0500

    [ADD] utils and dependencies

---
+++ dependencies/getUser.py
@@ -0,0 +1,41 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status, Security, Depends
+from fastapi.security import APIKeyHeader
+from typing import Optional
+from jose import JWTError, jwt
+from dotenv import load_dotenv
+from config.db import get_db
+from sqlalchemy import text
+import os
+
+
+load_dotenv()
+SECRET_KEY=os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+
+api_key_header = APIKeyHeader(name="Authorization", auto_error=False)
+
+def get_current_user(authorization: Optional[str] = Security(api_key_header), db: Session = Depends(get_db)):
+  if authorization is None or not authorization.startswith("Bearer "):
+    raise_error()
+  try:
+    token = authorization.split(" ")[1]
+    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    user_id = payload.get("userId")
+    if user_id is None:
+      raise_error()
+
+    user = db.execute(text("select 1 from user where id = :id"), {"id": user_id}).fetchone()
+    if not user:
+      return {"error": "User not found"}, 404
+
+    return {"id": user_id}
+  except JWTError:
+    raise_error()
+
+def raise_error():
+  raise HTTPException(
+      status_code=status.HTTP_401_UNAUTHORIZED,
+      detail="Could not validate credentials",
+      headers={"WWW-Authenticate": "Bearer"},
+    )
---
+++ utils/generateId.py
@@ -0,0 +1,4 @@
+from cuid2 import Cuid
+
+cuid_generator = Cuid(length=25)
+
