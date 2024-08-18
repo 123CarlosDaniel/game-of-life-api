a184a80782c787e7fc069453e2e0592d3e57ea92
cdcm <carloss_jt@hotmail.com>
2024-08-18 16:50:09 -0500

    [UPDATE] query.sql

--- sql/query.sql
+++ sql/query.sql
@@ -73,3 +73,37 @@ BEGIN
 	execute stmt;
     DEALLOCATE PREPARE stmt;
 END
+
+-- functions
+--% LimitClause
+CREATE DEFINER=`root`@`localhost` FUNCTION `LimitClause`(page_number int, per_page int) RETURNS varchar(255) CHARSET utf8mb4
+    DETERMINISTIC
+BEGIN
+	set @limit_clause = case 
+    when per_page >= 1 THEN CONCAT(' LIMIT ', per_page, ' OFFSET ', (page_number - 1) * per_page) 
+	ELSE ' '
+    end;
+    return @limit_clause;
+END
+
+
+--% OrderByClause
+CREATE DEFINER=`root`@`localhost` FUNCTION `OrderByClause`(table_name varchar(255), column_name varchar(255),  sort_direction varchar(4)) RETURNS varchar(255) CHARSET utf8mb4
+    DETERMINISTIC
+BEGIN
+	-- declare order_clause varchar(255);
+    set @order_clause = case
+		when lower(sort_direction) = 'asc' then concat(' order by ', table_name,'.', column_name, ' ASC')
+        when lower(sort_direction) = 'desc' then concat(' order by ', table_name,'.', column_name, ' DESC')
+        else ' '
+	end;
+RETURN @order_clause;
+END
+
+--% Pages
+CREATE DEFINER=`root`@`localhost` FUNCTION `Pages`(total_pages int, per_page int) RETURNS int
+    DETERMINISTIC
+BEGIN
+	set @pages = if(per_page >= 1, ceil(total_pages/per_page), 1);
+	RETURN @pages;
+END
