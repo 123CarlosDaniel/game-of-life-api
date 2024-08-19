24874b39a8d8f8411e95157a9d032e04d8a9089f
cdcm <carloss_jt@hotmail.com>
2024-08-18 19:11:27 -0500

    [UPDATE] query.sql add creationGet sp

--- sql/query.sql
+++ sql/query.sql
@@ -1,4 +1,4 @@
--- Creation stored procedure v1
+--% CreationGetAll stored procedure v1
 CREATE DEFINER=`root`@`localhost` PROCEDURE `creations_get_sp`(in page_number int, in per_page int, in sort_name varchar(4), out pages int)
 BEGIN
 	declare total_rows int default 0;
@@ -106,4 +106,31 @@ CREATE DEFINER=`root`@`localhost` FUNCTION `Pages`(total_pages int, per_page int
 BEGIN
 	set @pages = if(per_page >= 1, ceil(total_pages/per_page), 1);
 	RETURN @pages;
+END
+
+
+-- % CreationGet Stored Procedure
+CREATE DEFINER=`root`@`localhost` PROCEDURE `creation_get_sp`(in id varchar(191))
+BEGIN
+set @id = id;
+set @sql_query = concat(
+    'SELECT c.id as creation_id, ',
+    'uc.id as owner_id, ',
+    'uc.name as owner_name, ',
+    'uc.image as owner_image, ',
+    'c.title, ',
+    'c.description, ', 
+    'c.createdAt as creation_createdAt, ', 
+    'c.updatedAt as creation_updatedAt, ',
+    'COUNT(r.id) as reactions_count, ',
+    'COUNT(cm.id) AS comments_count ',
+    ' FROM creation c ',
+    'LEFT JOIN comment cm ON c.id = cm.creationId ',
+    'LEFT JOIN reaction r ON c.id = r.creationId ', 
+    'LEFT JOIN user uc ON c.ownerId = uc.id ',
+	'WHERE c.id = ? ',
+    'GROUP BY c.id, c.title, c.description, c.createdAt, c.updatedAt ');
+    PREPARE stmt FROM @sql_query;
+    EXECUTE stmt USING @id; 
+    DEALLOCATE PREPARE stmt;
 END
